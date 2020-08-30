936023890161110fa0408285cead89ff4718fc57
Tamires Rodrigues <tamires.guesdri@gmail.com>
2020-08-30 12:38:57 -0300

    Menu + intro
    Menu + intro

---
+++ Alpaca Ressurge/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+   public Canvas myCanvas;
+   public GameObject cena1;
+   public GameObject cena2;
+
+   public void Start()
+   {
+       myCanvas = myCanvas.GetComponent<Canvas>();
+       myCanvas.gameObject.SetActive(true);
+       cena1.gameObject.SetActive(true);
+       
+   }
+       public void PlayGame()
+   {
+      myCanvas.gameObject.SetActive(false);
+      cena1.gameObject.SetActive(false);
+
+   }
+
+   
+}
+   
